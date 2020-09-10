699fb3c8f71d5e779871a1123c04cb095adcdff0
sh3ra <youssefalshaarawi@yahoo.com>
2020-09-10 10:49:48 +0200

    leetcode

---
+++ Problem Solving/C++/leetcode/MinimumASCIIDeleteSumforTwoStrings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int dp[1007][1007];
+    int minimumDeleteSum(string s1, string s2) {
+        for(int i=s1.size()-1;i>=0;i--)
+        {
+            dp[i][s2.size()]=dp[i+1][s2.size()]+s1[i];
+        }
+        for(int i=s2.size()-1;i>=0;i--)
+        {
+            dp[s1.size()][i]=dp[s1.size()][i+1]+s2[i];
+        }
+        for(int i=s1.size()-1;i>=0;i--)
+        {
+            for(int j=s2.size()-1;j>=0;j--)
+            {
+                if(s1[i]==s2[j])
+                    dp[i][j]=dp[i+1][j+1];
+                else
+                {
+                    dp[i][j]=min({dp[i+1][j]+s1[i],dp[i][j+1]+s2[j]});
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
